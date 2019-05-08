5e27584dcf5469a8e81f3893d1c6f82646a9e3b2
xinru li <34546552+Stick-To@users.noreply.github.com>
2019-05-08 15:38:17 +0800

    upload utils

---
+++ utils/test_voc_utils.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import utils.tfrecord_voc_utils as voc_utils
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+tfrecord = voc_utils.dataset2tfrecord('../VOC/Annotations', '../VOC/JPEGImages',
+                                      '../data/', 'test', 10)
+print(tfrecord)
